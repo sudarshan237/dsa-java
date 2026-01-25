3a615495d3f82d7d92521beb1b43422b2c66b7c5
Sudarshan A Patil <patil.sudu2307@gmail.com>
2026-01-25 09:48:50 +0530

    Day 6: Count subarrays with sum K (brute force)

---
+++ Day06/CountSubarraysSumK.java
@@ -0,0 +1,18 @@
+public class CountSubarraysSumK {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 1};
+        int k = 5;
+
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+                if (sum == k) count++;
+            }
+        }
+
+        System.out.println("Count = " + count);
+    }
+}
